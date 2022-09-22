49abff19613503a9ccd8038c89b8860a6ba013ac
Mischa02011990 <111740472+Mischa02011990@users.noreply.github.com>
2022-09-22 17:15:43 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Components/Header";
+import {Navbar} from "./Components/Navbar";
+import {Profile} from "./Components/Profile";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            <Profile />
+        </div>
+    );
 }
+
 
 export default App;
